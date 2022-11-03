cba8d0d7f7e9b0b1c2abcba316ff0d6f929b46a4
euandoidge <83353856+euandoidge@users.noreply.github.com>
2022-11-03 14:50:02 +0000

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1201},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1201},"pages":[{"id":"page-nmr-spectroscopy","displayName":"NMR Spectroscopy","link":{"linkType":"LinkTypePage","href":"#!page-nmr-spectroscopy"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
