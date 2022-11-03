a8eed6b58551d871ab0d78020b54e575cef5b499
euandoidge <83353856+euandoidge@users.noreply.github.com>
2022-11-03 14:57:49 +0000

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1201},"pages":[{"id":"page-nmr-spectroscopy","displayName":"NMR Spectroscopy","link":{"linkType":"LinkTypePage","href":"#!page-nmr-spectroscopy"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
