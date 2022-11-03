285605c12d14832f4f083a39cde58dbc08cbf062
euandoidge <83353856+euandoidge@users.noreply.github.com>
2022-11-03 14:56:08 +0000

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1201},"pages":[{"id":"page-nmr-spectroscopy","displayName":"NMR Spectroscopy","link":{"linkType":"LinkTypePage","href":"#!page-nmr-spectroscopy"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
